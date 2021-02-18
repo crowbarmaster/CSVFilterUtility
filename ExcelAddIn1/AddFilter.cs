9744b167428c3b8e3daa4dde0cab0e8cc1011bbe
crowbarmaster <Freemanhl21.`>
2021-02-18 13:42:31 -0500

    Code cleanup.

--- ExcelAddIn1/AddFilter.cs
+++ ExcelAddIn1/AddFilter.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Windows.Forms;
 using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
 
 namespace ExcelAddIn1
 {
--- ExcelAddIn1/StartBtn.cs
+++ ExcelAddIn1/StartBtn.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.Office.Tools.Ribbon;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace ExcelAddIn1
 {
