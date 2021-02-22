acca9c464c417a72e681cbd247f36b13e0a23cc2
crowbarmaster <Crowbarmasterhl2@gmail.com>
2021-02-22 13:21:55 -0500

    Added ability to modify lower label. Min/Maximize Disables moved to designer properties.

--- Src/CancelDialog.cs
+++ Src/CancelDialog.cs
@@ -5,9 +5,21 @@ namespace ExcelAddIn1
 {
     public partial class CancelDialog : Form
     {
+        public string EditLblText;
         public CancelDialog()
         {
             InitializeComponent();
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Shown += CancelDialog_Shown;
+        }
+
+        private void CancelDialog_Shown(object sender, EventArgs e)
+        {
+            if(EditLblText != null)
+            {
+                editLbl.Text = EditLblText;
+            }
         }
 
         private void yesBtn_Click(object sender, EventArgs e)
@@ -19,6 +31,11 @@ namespace ExcelAddIn1
         private void noBtn_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Src/CancelDialog.designer.cs
+++ Src/CancelDialog.designer.cs
@@ -29,8 +29,9 @@ namespace ExcelAddIn1
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CancelDialog));
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.editLbl = new System.Windows.Forms.Label();
             this.yesBtn = new System.Windows.Forms.Button();
             this.noBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -39,21 +40,22 @@ namespace ExcelAddIn1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(52, 9);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(197, 24);
+            this.label1.Size = new System.Drawing.Size(217, 24);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Are you sure you wish";
+            this.label1.Text = "Are you sure you wish to";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // editLbl
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(41, 36);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(224, 24);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "to cancel this opperation?";
+            this.editLbl.AutoSize = true;
+            this.editLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editLbl.Location = new System.Drawing.Point(12, 33);
+            this.editLbl.Name = "editLbl";
+            this.editLbl.Size = new System.Drawing.Size(204, 24);
+            this.editLbl.TabIndex = 2;
+            this.editLbl.Text = "cancel this opperation?";
             // 
             // yesBtn
             // 
@@ -79,12 +81,17 @@ namespace ExcelAddIn1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(305, 121);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(313, 119);
             this.Controls.Add(this.noBtn);
             this.Controls.Add(this.yesBtn);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.editLbl);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "CancelDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "CancelDialog";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -94,7 +101,7 @@ namespace ExcelAddIn1
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label editLbl;
         private System.Windows.Forms.Button yesBtn;
         private System.Windows.Forms.Button noBtn;
     }
